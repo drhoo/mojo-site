093a2407f72b33b5188b5e6e2069b19b10a728ff
Brian Medlock <medlock@gmail.com>
2025-09-13 22:41:34 +1000

    Update contact.js

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,6 +1,7 @@
 // /pages/api/contact.js
 
-import { resend } from '../../lib/resend';
+import resend from '../lib/resend.js';
+
 import { createClient } from '@supabase/supabase-js';
 
 // Supabase setup
